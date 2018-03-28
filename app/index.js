0482d4f11b0cea63bad34f5a7bbb6da73f835423
jaevans36 <jaevans36@gmail.com>
2018-03-28 19:01:41 +0100

    Completely re-wrote gulpfile part 1

---
+++ app/index.js
@@ -0,0 +1,1 @@
+import mainJs from '../app/scripts/main';
--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,273 @@
+// Imports and requires
+
 const gulp = require('gulp'),
 			browserSync = require('browser-sync').create(),
 			gutil = require('gulp-util'),
 			sass = require('gulp-sass'),
 			uglify = require('gulp-uglify'),
+			beautify = require('gulp-beautify'),
+			notify = require('gulp-notify'),
 			concat = require('gulp-concat'),
+			plumber = require('gulp-plumber'),
+			cssimport = require('gulp-cssimport'),
+			sourcemaps = require('gulp-sourcemaps'),
 			eslint = require('gulp-eslint'),
 			autoprefixer = require('gulp-autoprefixer'),
+			imagemin = require('gulp-imagemin'),
 			minify = require('gulp-minify-css'),
-			babel = require('gulp-babel');
+			babel = require('gulp-babel'),
+			ftp = require('vinyl-ftp');
 
-var sassOptions = {
-	errLogToConsole: true,
-	outputStyle: 'expanded'
-}
+// Configs
 
-// Static server and watching scss + html files
-gulp.task('start', ['sass','html'], () => {
+const base = {
+		dist: 'dist/',
+		app: 'app/',
+		assets: 'dist/assets'
+};
 
-  browserSync.init({
-    server: "./app"
-  });
+const routes = {
+	styles: {
+		scss: `${base.app}styles/*.scss`,
+		_scssinc: `${base.app}styles/_includes/*.scss`,
+		css: `${base.dist}assets/css/`
+	},
+	scripts: {
+		base: `${base.app}scripts/`,
+		js: `${base.app}scripts/*.js`,
+		jsmin: `${base.dist}assets/js/`
+	},
+	templates: {
+		pug: `${base.src}templates/*.pug`,
+		_pug: `${base.src}templates/_includes/*.pug`
+	},
+	files: {
+		html: 'dist/',
+		images: `${base.app}images/*`,
+		imgmin: `${baseDirs.dist}assets/files/img/`,
+		cssFiles: `${base.dist}assets/css/*.css`,
+		htmlFiles: `${base.dist}assets/*.html`,
+		styleCss: `${base.dist}css/style.css`
+	},
+	deploy: {
+		baseDir: base.dist,
+		baseDirFiles: `${base.dist}**`,
+		ftpUploadDir: '[FTP-DIRECTORY]'
+	}
+};
 
-  gulp.watch("app/**/*.js", ['js'])
-      .on('change', browserSync.reload);
-  gulp.watch("app/styles/*.scss", ['sass'])
-      .on('change', browserSync.reload);
-  gulp.watch("app/*.html", ['html'])
-      .on('change', browserSync.reload);
+// FTP Credentials
 
-  gutil.log(gutil.colors.green('Server started!'));
+const ftpCreds = {
+	host: '[HOST]',
+	user: '[USER]',
+	password: '[PASSWORD]'
+};
+
+const surgeInfo = {
+	domain: '[DOMAIN.surge.sh]' // http://surge.sh/help/getting-started-with-surge
+};
+
+// Sass config
+
+const sassOptions = {
+	outputStyle: 'compressed'
+};
+
+/* ### Compile tasks ### */
+
+// Pug
+/*
+	Tasks:
+	Unpipe stream on error
+	Start sourcemap process
+	Sass compiler
+	Autoprefixer
+	Complete sourcemap process
+	Replace improt statements with css
+	Rename file to 'style.css'
+	Copy file to distribution folder
+	Browsersync inject changes without page refresh
+	Notify user that Styles task is completed successfully
+*/
+gulp.task('templates', () => {
+	return gulp.src([routes.templates.pug, '!' + routes.templates._pug])
+		.pipe(plumber({
+			errorHandler: notify.onError({
+				title: 'Error: Compiling Pug failed.',
+				message: '<%= error.message %>'
+			})
+		}))
+		.pipe(pug())
+		.pipe(gulp.dest(routes.files.html))
+		.pipe(browserSync.reload())
+		.pipe(notify({
+			title: 'Pug Compiled successfully.',
+			message: 'templates task completed.'
+		}));
+})
+
+// SCSS
+/*
+	Tasks:
+	Unpipe stream on error
+	Start sourcemap process
+	Sass compiler
+	Autoprefixer
+	Complete sourcemap process
+	Replace improt statements with css
+	Rename file to 'style.css'
+	Copy file to distribution folder
+	Browsersync inject changes without page refresh
+	Notify user that Styles task is completed successfully
+*/
+gulp.task('styles', () => {
+	return gulp.src(routes.styles.scss)
+		.pipe(plumber({
+			errorHandler: notify.onError({
+				title: 'Error: Compiling SCSS failed.',
+				message: '<%= error.message %>'
+			})
+		}))
+		.pipe(sourcemaps.init())
+		.pipe(sass(sassOptions))
+		.pipe(autoprefixer('last 3 versions'))
+		.pipe(sourcemaps.write())
+		.pipe(cssimport({}))
+		.pipe(rename('style.css'))
+		.pipe(gulp.dest(routes.styles.css))
+		.pipe(browserSync.stream())
+		.pipe(notify({
+			title: 'SCSS Compiled and Minified successfully.',
+			message: 'scss task completed.'
+		}));
 });
 
-gulp.task('js', () => {
-	gulp.src('app/**/*.js')
-			.pipe(babel({
-				presets:['env']
-			}))
-      .pipe(uglify())
-      .pipe(concat('script.js'))
-      .pipe(gulp.dest('dist/script'));
-
-  gutil.log(gutil.colors.green('JS compiled successfuly!'));
+// Scripts
+/*
+	Tasks:
+	Unpipe stream on error
+	Start sourcemap process
+	Concatenate script.js
+	Convert ES6 => ES5
+	Minify the code to save space
+	Complete sourcemap process
+	Copy the file to distribution folder
+	Copy file to css folder
+	Browsersync refresh the page
+	Notify user that Scripts task is completed successfully
+*/
+gulp.task('scripts', () => {
+	return gulp.src(routes.scripts.js)
+		.pipe(plumber({
+			errorHandler: notify.onError({
+				title: 'Error: Babel and Concat failed.',
+				message: '<%= error.message %>'
+			})
+		}))
+		.pipe(sourcemaps.init())
+		.pipe(concat('script.js'))
+		.pipe(bable())
+		.pipe(uglify())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(routes.scripts.jsmin))
+		.pipe(browserSync.reload())
+		.pipe(notify({
+			title: 'JavaScript minified and concatenated.',
+			message: 'js files have been minified and concatenated.'
+		}));
 });
 
-gulp.task('lint', () => {
-  gulp.src(['**/*.js', '!node_modules/**'])
-      .pipe(eslint())
-      .pipe(eslint.format())
-      .pipe(eslint.failAfterError());
+// Images
+/*
+	Tasks:
+	Minify the images
+	Copy the minified images to distribution folder
+*/
+gulp.task('images', () => {
+	gulp.src(routes.files.images)
+		.pipe(imagemin())
+		.pipe(gulp.dest(routes.files.imgmin))
 });
 
-gulp.task('sass', () => {
-  gulp.src('app/styles/*.scss')
-			.pipe(autoprefixer({
-				browsers: ['last 2 versions'],
-				cascade: false
-			}))
-			.pipe(sass(sassOptions).on('error', sass.logError))
-			.pipe(minify())
-      .pipe(gulp.dest('dist/css'))
+// Deploy
+/*
+	Tasks:
+	Create the connection using the ftp config
+	Unpipe stream on error
+	Upload all files to prod
+	Notify user that Deploy task is completed successfully
+*/
+gulp.task('upload', () => {
+	const connection = ftp.create({
+		host: ftpCreds.host,
+		user: ftpCreds.user,
+		password: ftpCreds.password
+	});
 
-  gutil.log(gutil.colors.green('CSS compiled successfuly!'));
+	return gulp.src(routes.deploy.baseDirFiles, {
+		base: routes.deploy.baseDir,
+		buffer: false
+	})
+	.pipe(plumber({
+		errorHandler: notify.onError({
+			title: 'Error: Deployment failed.',
+			message: '<%= error.message %>'
+		})
+	}))
+	.pipe(connection.dest(routes.deploy.ftpUploadDir))
+	.pipe(notify({
+		title: 'Deployment successful.',
+		message: 'Deployment completed.'
+	}));
 });
 
-gulp.task('html', function() {
-  gulp.src('app/index.html')
-      .pipe(gulp.dest('dist'))
-      .on('error', gutil.log);
-
-  gutil.log(gutil.colors.green('HTML compiled successfuly!'));
+/*
+	Tasks:
+	Publish site to surge
+*/
+gulp.task('surge', () => {
+	return surge({
+		project: routes.deploy.baseDir,
+		domain: [SURGEINFO.DOMAIN]
+	})
+	.pipe(notify({
+		title: 'Surge deployment successful.',
+		message: 'Deployment to Surge completed.'
+	}));
 });
 
-// Manually run all tasks
-// gulp.task('default', ['copy','sass','js']);
+// Pre-production tasks
+/*
+	Tasks:
+	Beautify script files with 2 indents
+	Unpipe stream on error
+	Copy the beautified files
+	Notify user that beautification is completed successfully
+*/
+gulp.task('beautify', () => {
+	return gulp.src(routes.scripts.js)
+		.pipe(beautify({indentSize: 2}))
+		.pipe(plumber({
+			errorHandler: notify.onError({
+				title: 'Error: Beautify failed.',
+				message: '<%= error.message %>'
+			})
+		}))
+		.pipe(gulp.dest(routes.scripts.base))
+		.pipe(notify({
+			title: 'Beautification successful.',
+			message: 'Beautification of scripts successful.'
+		}));
+});
+
+// Create local server and watch for changes
+gulp.task('watch', () => {
+	browserSync.init({
+		server: './dist/'
+	});
+
+	gulp.watch([routes.styles.scss, routes.styles._scss], ['styles']);
+	// PUG WATCH TASK
+	gulp.watch(routes.scripts.js, ['scripts', 'beautify']);
+});
